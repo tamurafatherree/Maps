32d82cfc7714ec690fc4466c0b44369a88a7cf8c
Tamura Fatherree <ctamtri@gmail.com>
2016-11-28 17:43:42 -0800

    Add files via upload

---
+++ js/nls/sr/resources.js
@@ -0,0 +1,42 @@
+﻿/*global define */
+/*
+ | Copyright 2014 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
+define({
+  "map": {
+    "error": "Kreiranje mape nije moguće"
+  },
+  "tools": {
+    "bookmarkTool": "Obeleživači",
+    "basemapTool": "Pozadinske mape",
+    "legendTool": "Legenda",
+    "detailsTool": "Detalji",
+    "measureTool": "Merenje",
+    "shareTool": "Podeli",
+    "layerTool": "Slojevi",
+    "tableTool": "Tabela",
+    "editTool": "Izmeni",
+    "printTool": "Štampaj",
+    "printLegend": "Dodaj legendu u rezultate",
+    "printLayouts": {
+      "label1": "Položeno",
+      "label2": "Uspravno",
+      "label3": "Položeno",
+      "label4": "Uspravno"
+    },
+    "printTitle": "Naslov",
+    "printTitlePrompt": "Dodaj prilagođeni naslov"
+  }
+});
---
+++ js/nls/zh-hk/resources.js
@@ -0,0 +1,42 @@
+﻿/*global define */
+/*
+ | Copyright 2014 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
+define({
+  "map": {
+    "error": "無法建立地圖"
+  },
+  "tools": {
+    "bookmarkTool": "書簽",
+    "basemapTool": "底圖",
+    "legendTool": "圖例",
+    "detailsTool": "詳細資訊",
+    "measureTool": "測量",
+    "shareTool": "分享",
+    "layerTool": "圖層",
+    "tableTool": "表",
+    "editTool": "編輯",
+    "printTool": "列印",
+    "printLegend": "在輸出新增圖例",
+    "printLayouts": {
+      "label1": "橫向",
+      "label2": "縱向",
+      "label3": "橫向",
+      "label4": "縱向"
+    },
+    "printTitle": "標題",
+    "printTitlePrompt": "新增自訂標題"
+  }
+});
